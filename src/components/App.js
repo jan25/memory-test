a7baa89644ac99a7f5caefce684503513455ce07
Abhilash Gnan <abhilashgnan@gmail.com>
2020-03-04 00:11:34 +0100

    Adding number component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
+import NumberCard from "./NumberCard";
 
 class App extends Component {
   state = {};
   render() {
-    return <div>This is App Component</div>;
+    const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    return (
+      <React.Fragment>
+        <div id="game-main">
+          This is App Component
+          {number.map(n => (
+            <NumberCard num={n} />
+          ))}
+        </div>
+        <div id="game-info">
+          <h4>Click on 1 to start the test</h4>
+          <p>
+            Goal: remember the layout of numbers 1-9 on this page. The numbers
+            are turned upside down when you click on number 1. And you have to
+            recall the remaining numbers in order by clicking on upside down
+            cards.
+          </p>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 

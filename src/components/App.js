96cd0ff9fc8d9f357368ba5be5aa5d3d9d86e22d
Abhilash Gnan <abhilashgnan@gmail.com>
2020-03-03 23:08:35 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div>This is App Component</div>;
+  }
+}
+
+export default App;
